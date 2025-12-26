deebdd602617645575d7121467d8a295a607d541
erikk <Erik.k.braun@gmail.com>
2025-12-26 20:49:46 +0100

    databs project fortsättning

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,5 +1,12 @@
 package controller;
 
+/**
+ * ALLA KRAV SOM ÄR KVAR:
+ * admin:
+ * r-8-11, r14
+ * User:
+ * r5, r10, ,r11(discount part), r13.
+ */
 
 public class Main {
     public static void main(String[] args) throws Exception {
